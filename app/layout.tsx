b128ff34a524e9e0792af560dc318c41e61b3279
SHØ <neila@users.noreply.github.com>
2023-04-25 01:34:51 +0900

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-import './globals.css';
+import './style.linaria.global';
+import { Inter } from 'next/font/google';
+
+const fontInter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="ja" className={fontInter.className}>
       <head />
       <body>{children}</body>
     </html>
