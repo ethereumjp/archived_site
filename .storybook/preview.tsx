89df64c186ec732d4332640182912fcfe3bd6040
SHØ <neila@users.noreply.github.com>
2023-04-25 04:43:24 +0900

    add storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,37 @@
+import * as NextImage from 'next/image';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import React from 'react';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/iFsEDE9jCVe1mANQZVrcOD',
+  },
+};
+
+export const decorators = [
+  (Story) => {
+    return (
+      <>
+        <Story />
+      </>
+    );
+  },
+];
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
