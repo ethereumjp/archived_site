2de1ff2ff01dfb0340512d275872d75132d5ce34
SHØ <neila@users.noreply.github.com>
2023-04-25 01:35:58 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withLinaria = require('next-with-linaria');
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: { appDir: true },
 };
 
-module.exports = nextConfig;
+module.exports = withLinaria(nextConfig);
