3087e91f2f95c027d327405346d10a680e7b5cdd
SHØ <neila@users.noreply.github.com>
2023-05-22 23:58:33 +0900

    add stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,18 +1,20 @@
 const styleLintConfig = {
-  extends: ['@linaria/stylelint-config-standard-linaria'],
+  extends: ['stylelint-config-standard', 'stylelint-config-styled-components'],
+  customSyntax: 'postcss-styled-syntax',
+  plugins: ['stylelint-order'],
   overrides: [
     {
       files: ['src/**/*.{jsx,tsx}'],
     },
   ],
-  plugins: ['stylelint-order'],
   rules: {
+    'block-no-empty': null,
+    'declaration-property-value-no-unknown': true,
+    'function-no-unknown': null,
     'order/properties-alphabetical-order': true,
     'no-empty-source': null,
     'no-descending-specificity': null,
     'rule-empty-line-before': null,
-    'block-no-empty': null,
-    'function-no-unknown': null,
     'selector-pseudo-class-no-unknown': [
       true,
       {
