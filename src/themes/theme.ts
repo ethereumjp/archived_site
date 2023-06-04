2ab6ed489fd4d24872e9881eb16fec39cdd43297
SHØ <neila@users.noreply.github.com>
2023-06-04 09:45:04 +0900

    add basic themes

---
+++ src/themes/theme.ts
@@ -0,0 +1,11 @@
+import { mq } from './settings/breakpoints';
+import { colorScheme } from './settings/color';
+import { zIndex } from './settings/zindex';
+
+export const themes = (mode: 'light' | 'dark') =>
+  mode === 'light' ? colorScheme.themeLight : colorScheme.themeDark;
+
+export const theme = {
+  breakpoint: mq,
+  zindex: zIndex,
+};
