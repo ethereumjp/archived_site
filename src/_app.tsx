e85ed5c8a5dada911a144c71d462a65f653dc9e0
neila <neila@shiftbase.xyz>
2023-06-14 11:55:05 +0900

    format and regenerate lockfile

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,7 +1,20 @@
+// import createCache from '@emotion/cache';
+// import { CacheProvider, Global, ThemeProvider, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
+// import { themeDark, themeLight } from '@/themes/settings/color';
+
+// const styleCache = createCache({ key: 'next' });
+
 const EthJpSite = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    // <CacheProvider value={styleCache}>
+    //   <ThemeProvider theme={true ? themeDark : themeLight}>
+    //     <Global styles={css``} />
+    <Component {...pageProps} />
+    //   </ThemeProvider>
+    // </CacheProvider>
+  );
 };
 
 export default EthJpSite;
