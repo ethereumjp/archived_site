97ade4edb68c91a3bf18e5eb895ea67363fca21d
neila <neila@shiftbase.xyz>
2023-06-14 15:50:07 +0900

    remove files

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-const devices = ['mobile', 'tablet', 'laptop', 'desktop', 'monitor'];
-const breakpoints = [480, 768, 1024, 1440, 2560];
-
-export const mq = Object.fromEntries(
-  devices.map((_, i) => [
-    devices[i],
-    `@media (min-width: ${breakpoints[i]}px)`,
-  ]),
-);
