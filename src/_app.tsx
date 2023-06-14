1355da5ccd885d3efc1c16439f52fe6e3c5ff871
neila <neila@shiftbase.xyz>
2023-06-14 15:48:43 +0900

    add global theme

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,20 +1,7 @@
-// import createCache from '@emotion/cache';
-// import { CacheProvider, Global, ThemeProvider, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
-// import { themeDark, themeLight } from '@/themes/settings/color';
-
-// const styleCache = createCache({ key: 'next' });
-
 const EthJpSite = ({ Component, pageProps }: AppProps) => {
-  return (
-    // <CacheProvider value={styleCache}>
-    //   <ThemeProvider theme={true ? themeDark : themeLight}>
-    //     <Global styles={css``} />
-    <Component {...pageProps} />
-    //   </ThemeProvider>
-    // </CacheProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default EthJpSite;
--- src/themes/global.ts
+++ src/themes/global.ts
@@ -1,6 +1,45 @@
-import { css } from '@emotion/react';
-
+import { mq } from './settings/breakpoints';
 import {} from './settings/color';
 import {} from './settings/spaces';
 
-export const globalStyles = css``;
+import { css } from '@emotion/react';
+
+export const globalStyles = css({
+  'html, body': {},
+
+  h1: {
+    [mq.desktop]: {
+      fontSize: '4.5rem',
+      lineHeight: 1.1,
+    },
+  },
+
+  h2: {
+    [mq.desktop]: {
+      fontSize: '3rem',
+      lineHeight: 1.15,
+    },
+  },
+
+  h3: {
+    [mq.desktop]: {
+      fontSize: '2.25rem',
+      lineHeight: 1.25,
+    },
+  },
+
+  h4: {
+    [mq.desktop]: {
+      fontSize: '1.1rem',
+      lineHeight: 1.1,
+    },
+  },
+
+  p: {},
+
+  a: {
+    textDecoration: 'none',
+  },
+
+  '*': {},
+});
