1a1aa67411e9a4db5f62b759e04fe0d049878a9a
neila <neila@shiftbase.xyz>
2023-09-03 02:36:02 +0900

    add about section

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,15 +1,15 @@
 import { NextPage } from 'next';
 
-import RootLayout from '@/components/layouts/base';
+import Layout from '@/components/layouts/base';
 import type { PageProps } from '@/types';
 
 const Events: NextPage<PageProps> = ({ params, searchParams }) => {
   return (
-    <RootLayout pageTitle='events'>
+    <Layout pageTitle='events'>
       <div>
         <h1>Events</h1>
       </div>
-    </RootLayout>
+    </Layout>
   );
 };
 
