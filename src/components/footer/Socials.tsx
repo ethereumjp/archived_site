3c0a53977a6b28f542a240a71ebbb91bd5f64f70
neila <neila@shiftbase.xyz>
2023-09-03 21:27:58 +0900

    slim social

--- src/components/footer/Socials.tsx
+++ src/components/footer/Socials.tsx
@@ -14,24 +14,24 @@ export const Socials = () => {
     <div css={container}>
       <SocialIcon
         url='https://github.com/ethereumjp'
-        bgColor='#000000'
-        fgColor='transparent'
+        bgColor='transparent'
+        fgColor='#000000'
         label='Github'
         target='_blank'
         rel='noreferrer'
       />
       <SocialIcon
         url='https://twitter.com/Ethereum_JP'
-        bgColor='#1DA1F2'
-        fgColor='transparent'
+        bgColor='transparent'
+        fgColor='#1DA1F2'
         label='Twitter'
         target='_blank'
         rel='noreferrer'
       />
       <SocialIcon
         url='https://discord.gg/Tm5jU3DSCE'
-        bgColor='#7289DA'
-        fgColor='white'
+        bgColor='white'
+        fgColor='#7289DA'
         label='Discord'
         target='_blank'
         rel='noreferrer'
