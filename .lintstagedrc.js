3574cb59ea7b87cf73738bb8cdbbba3952f63b58
neila <40727091+neila@users.noreply.github.com>
2024-01-15 19:25:47 +0900

    replace rome with biome

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 const buildLintCommand = (filenames) =>
-  `yarn run rome check ${filenames
+  `yarn run biome lint ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+    .join(" ")}`;
 
 const buildFormatCommand = (filenames) =>
-  `yarn run rome format ${filenames
+  `yarn run biome format ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' ')} `;
+    .join(" ")} `;
 
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': [buildLintCommand],
-  '**/*.{js,jsx,ts,tsx,json}': [buildFormatCommand],
+  "**/*.{js,jsx,ts,tsx}": [buildLintCommand],
+  "**/*.{js,jsx,ts,tsx,json}": [buildFormatCommand],
 };
